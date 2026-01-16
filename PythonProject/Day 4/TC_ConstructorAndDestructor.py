d7b6d0878f054900818943c8f2b1be1d6628c03b
Veera manoj <145892518+Gummadiveeramanoj@users.noreply.github.com>
2026-01-16 18:01:15 +0530

    Add files via upload

---
+++ PythonProject/Day 4/TC_ConstructorAndDestructor.py
@@ -0,0 +1,7 @@
+class employee:
+    def __init__(self,name):
+        self.name = name
+        print("constructor is called")
+    def __del__(self):
+        print("destructor is called")
+e=employee("James")
