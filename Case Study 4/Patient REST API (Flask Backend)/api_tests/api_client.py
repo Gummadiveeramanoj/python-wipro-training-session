697e33c96e6d3d586a986b420fe8a439a642e0b6
Veera manoj <145892518+Gummadiveeramanoj@users.noreply.github.com>
2026-02-03 17:46:13 +0530

    Delete Case Study 4 directory

--- Case Study 4/Patient REST API (Flask Backend)/api_tests/api_client.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-BASE_URL = "http://127.0.0.1:5000/api/patients"
-
-def get_patients():
-    return requests.get(BASE_URL)
-
-def add_patient(payload):
-    return requests.post(BASE_URL, json=payload)
--- Case Study 4/Patient REST API (Flask Backend)/conftest.py
+++
@@ -1,6 +0,0 @@
-import pytest
-import requests
-
-@pytest.fixture
-def base_url():
-    return "http://127.0.0.1:5000/api/patients"
